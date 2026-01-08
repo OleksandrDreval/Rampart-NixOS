ddffe72735608b20364b8fffd3deeca5af14fcd5
OleksandrDreval <180102593+OleksandrDreval@users.noreply.github.com>
2026-01-08 19:00:30 +0200

    feat(modules): Add configuration for desktop environments with GNOME and PipeWire support

---
+++ modules/desktop.nix
@@ -0,0 +1,41 @@
+{ config, pkgs, ... }:
+
+let
+  vars = import ./variables.nix;
+in
+{
+  # Enable X11 windowing system
+  services.xserver.enable = true;
+
+  # Enable GNOME Desktop Environment
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = vars.xkbLayout;
+    variant = "";
+  };
+
+  # Enable touchpad support (enabled by default in most desktop managers)
+  # services.xserver.libinput.enable = true;
+
+  # Enable printing support
+  services.printing.enable = true;
+
+  # Sound configuration with PipeWire
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    # jack.enable = true;
+
+    # Use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    # media-session.enable = true;
+  };
+}
