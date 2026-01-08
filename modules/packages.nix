ad9ab74f1593214da7dff7af87e8a4f0fed188d6
OleksandrDreval <180102593+OleksandrDreval@users.noreply.github.com>
2026-01-08 19:03:26 +0200

    feat(modules): Add initial configuration for system packages

---
+++ modules/packages.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+
+{
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Enable Firefox browser
+  programs.firefox.enable = true;
+
+  # System-wide packages
+  environment.systemPackages = with pkgs; [
+    # Add your packages here
+    # vim
+    # wget
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # Enable the OpenSSH daemon
+  # services.openssh.enable = true;
+}
