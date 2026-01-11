f44fe145e17a5a7f84d02c5808b34433a675bdf7
OleksandrDreval <180102593+OleksandrDreval@users.noreply.github.com>
2026-01-11 12:13:55 +0200

    feat(modules/users): Add sudo timestamp timeout configuration for enhanced security

--- modules/users.nix
+++ modules/users.nix
@@ -14,4 +14,16 @@ in
       # thunderbird
     ];
   };
+
+  # Sudo security configuration
+  security.sudo = {
+    enable = true;
+    execWheelOnly = true;  # Only wheel group members can use sudo
+    extraConfig = ''
+      Defaults insults                                                  # Fun insults for wrong passwords
+      Defaults passwd_timeout=${toString vars.sudoPasswdTimeout}        # Time to enter password (seconds)
+      Defaults timestamp_timeout=${toString vars.sudoTimestampTimeout}  # Sudo cache duration (minutes)
+      Defaults use_pty                                                  # Use PTY for all sudo commands (prevents TTY hijacking)
+    '';
+  };
 }
