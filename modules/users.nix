32055485404d29203a6a2e98127d28c45b924e3c
OleksandrDreval <180102593+OleksandrDreval@users.noreply.github.com>
2026-01-13 22:35:30 +0200

    feat(modules/users): Prevent sudo usage without password by adding !visiblepw option

--- modules/users.nix
+++ modules/users.nix
@@ -37,6 +37,7 @@ in
       Defaults use_pty                                                  # Use PTY for all sudo commands (prevents TTY hijacking)
       Defaults env_reset                                                # Reset environment to secure baseline
       Defaults secure_path="/run/wrappers/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"  # Secure PATH
+      Defaults !visiblepw                                               # Never allow sudo if password not required
     '';
   };
 }
