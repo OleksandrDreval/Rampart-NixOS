fb987797b87d30be14228839b3793d65175224e2
OleksandrDreval <180102593+OleksandrDreval@users.noreply.github.com>
2026-01-15 16:28:37 +0200

    feat(modules/boot): Disable boot parameter editing to prevent init=/bin/sh attacks

--- modules/boot.nix
+++ modules/boot.nix
@@ -7,6 +7,7 @@ in
   # Bootloader configuration
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = vars.bootConfigLimit;
+  boot.loader.systemd-boot.editor = false;  # Disable boot parameter editing (prevent init=/bin/sh attacks)
   boot.loader.efi.canTouchEfiVariables = false;
   boot.loader.timeout = vars.bootTimeout;
 
