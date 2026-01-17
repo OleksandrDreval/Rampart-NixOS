d1c23bf01e1a76aa4d938d1a70c32689afc6fd54
OleksandrDreval <180102593+OleksandrDreval@users.noreply.github.com>
2026-01-17 16:41:06 +0200

    refactor(boot): Use lib.mkForce for bootloader and verbosity settings

--- modules/boot.nix
+++ modules/boot.nix
@@ -7,18 +7,18 @@ in
   # Bootloader configuration
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = vars.bootConfigLimit;
-  boot.loader.systemd-boot.editor = false;  # Disable boot parameter editing (prevent init=/bin/sh attacks)
-  boot.loader.efi.canTouchEfiVariables = false;
+  boot.loader.systemd-boot.editor = lib.mkForce false;  # Disable boot parameter editing (prevent init=/bin/sh attacks)
+  boot.loader.efi.canTouchEfiVariables = lib.mkForce false;
   boot.loader.timeout = vars.bootTimeout;
 
   # Boot verbosity configuration (security: minimize information disclosure)
-  boot.consoleLogLevel = 3;     # Show only errors on console (balance security/debugging)
-  boot.initrd.verbose = false;  # Quiet initrd to minimize information disclosure
+  boot.consoleLogLevel = lib.mkForce 3;     # Show only errors on console (balance security/debugging)
+  boot.initrd.verbose = lib.mkForce false;  # Quiet initrd to minimize information disclosure
 
   # LUKS encryption for swap
   boot.initrd.luks.devices."luks-${vars.luksSwapUUID}".device = "/dev/disk/by-uuid/${vars.luksSwapUUID}";
 
   # DMA attack mitigation during early boot
   # Blocks Thunderbolt/USB4 access in initrd to protect LUKS keys
-  boot.initrd.luks.mitigateDMAAttacks = true;
+  boot.initrd.luks.mitigateDMAAttacks = lib.mkForce true;
 }
