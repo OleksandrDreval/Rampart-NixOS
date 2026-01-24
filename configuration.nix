bfc325118f398fd21bd6f87a93d220376c92410d
OleksandrDreval <180102593+OleksandrDreval@users.noreply.github.com>
2026-01-24 23:17:10 +0200

    feat(configuration): Add rampart option declarations to system modules

--- configuration.nix
+++ configuration.nix
@@ -29,6 +29,7 @@ in
 
       # System modules
       ./modules/kernel.nix             # Kernel parameters, modules and sysctl settings
+      ./modules/rampart-options.nix    # Rampart option declarations
       ./modules/memory.nix             # Anything related to system memory (RAM)
       ./modules/entropy.nix            # Entropy / RNG configuration
       ./modules/filesystems.nix        # Filesystem-related sysctl and ntfs support
