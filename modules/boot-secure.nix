6040527e08af31b050be47ce7b824e5bba5809f2
OleksandrDreval <180102593+OleksandrDreval@users.noreply.github.com>
2026-01-25 19:25:46 +0200

    refactor(boot-secure): Remove LUKS device mappings from boot-secure.nix

--- modules/boot-secure.nix
+++ modules/boot-secure.nix
@@ -43,9 +43,6 @@ in
     configurationLimit = vars.bootConfigLimit;
   };
 
-  # LUKS device mappings carried from hardware-configuration.nix
-  boot.initrd.luks.devices."luks-${vars.luksRootUUID}".device = "/dev/disk/by-uuid/${vars.luksRootUUID}";
-
   # DMA attack mitigation during early boot
   # Blocks Thunderbolt/USB4 access in initrd to protect LUKS keys
   boot.initrd.luks.mitigateDMAAttacks = lib.mkForce true;
