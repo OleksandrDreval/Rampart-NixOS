fbc71e870fcf8778d713055e575894f72e8ade54
OleksandrDreval <180102593+OleksandrDreval@users.noreply.github.com>
2026-02-01 21:52:51 +0200

    feat(kernel): Enable unprivileged user namespaces for rootless containers and sandboxes

--- modules/kernel.nix
+++ modules/kernel.nix
@@ -115,6 +115,8 @@ let
     
     # ptrace restrictions
     "kernel.yama.ptrace_scope"           = 2;              # Maximum ptrace restrictions - admin only
+
+    "kernel.unprivileged_userns_clone"   =1;               # Enable unprivileged user namespaces (required for rootless containers and some sandboxes)
   };
 in
 
