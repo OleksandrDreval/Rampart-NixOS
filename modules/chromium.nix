2e87b6052c15c91cb1fe236f0d7fca5236d5b54d
OleksandrDreval <180102593+OleksandrDreval@users.noreply.github.com>
2026-02-01 08:55:05 +0200

    feat(chromium): Ensure unprivileged namespaces are available for sandboxing

--- modules/chromium.nix
+++ modules/chromium.nix
@@ -10,6 +10,9 @@ in
   # `users.users.<name>.packages` set elsewhere.
   users.users.${vars.mainUser}.packages = lib.mkDefault (with pkgs; [ ungoogled-chromium ]);
 
+  # Ensure unprivileged namespaces available for sandboxing
+  security.unprivilegedUsernsClone = lib.mkForce true;
+
   # Allow user namespaces for sandboxing
   security.allowUserNamespaces = lib.mkForce true;
 
