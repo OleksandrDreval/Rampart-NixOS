45350b829bae4bc33aa4a580cc6f62bcc533e04f
OleksandrDreval <180102593+OleksandrDreval@users.noreply.github.com>
2026-02-13 14:57:53 +0200

    fix(modules\apps\encryption\veracrypt): Update variable import to use centralized vars from SOPS secrets

--- modules/apps/encryption/veracrypt.nix
+++ modules/apps/encryption/veracrypt.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }:
 
 let
-  vars = import ./includes/variables.nix;
+  vars = import ../../security/secrets/vars-compat.nix { inherit config lib; };
 in
 {
   # allowUnfree is configured in `modules/packages.nix` so package sets
