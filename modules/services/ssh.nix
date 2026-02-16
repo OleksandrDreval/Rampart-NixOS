4e96f09b4b3ba79fe64884c5e0694591aa41930e
OleksandrDreval <180102593+OleksandrDreval@users.noreply.github.com>
2026-02-16 14:18:51 +0200

    feat(modules\services\ssh): Add additional post-quantum KexAlgorithms for enhanced security

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -36,7 +36,8 @@
       ];
 
       KexAlgorithms = [
-        "sntrup761x25519-sha512@openssh.com" # Постквантовий захист
+        "mlkem768x25519-sha256"
+        "sntrup761x25519-sha512@openssh.com"
         "curve25519-sha256"
         "curve25519-sha256@libssh.org"
       ];
