8035a08f832d31aac0dea5f29ec2781954721c95
OleksandrDreval <180102593+OleksandrDreval@users.noreply.github.com>
2026-02-16 13:50:20 +0200

    feat(modules\services\ssh): Add secure ciphers and post-quantum KexAlgorithms for enhanced security

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -28,10 +28,17 @@
         "hmac-sha2-256-etm@openssh.com"
         "umac-128-etm@openssh.com"
       ];
+
       Ciphers = [
         "chacha20-poly1305@openssh.com"
         "aes256-gcm@openssh.com"
         "aes128-gcm@openssh.com"
+      ];
+
+      KexAlgorithms = [
+        "sntrup761x25519-sha512@openssh.com" # Постквантовий захист
+        "curve25519-sha256"
+        "curve25519-sha256@libssh.org"
       ];
     };
 
