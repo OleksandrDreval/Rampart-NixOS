6c5ea0266a9cc7911a65c54ce5ffac0cacfd8111
OleksandrDreval <180102593+OleksandrDreval@users.noreply.github.com>
2026-02-16 13:23:16 +0200

    feat(modules\services\ssh): Enable MaxAuthTries setting for SSH configuration

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -12,7 +12,7 @@
       PasswordAuthentication = false;              # Only SSH keys, no passwords
       KbdInteractiveAuthentication = false;        # Disable keyboard-interactive auth
     #   X11Forwarding = false;                     # Disable X11/Wayland forwarding
-    #   MaxAuthTries = 3;                          # Maximum authentication attempts
+     MaxAuthTries = 3;                             # Maximum authentication attempts
     #   PubkeyAuthentication = true;               # Enable public key authentication
     #   AuthenticationMethods = "publickey";       # Only public key auth allowed
     #   LogLevel = "VERBOSE";                      # Detailed logging
