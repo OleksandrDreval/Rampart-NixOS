a8a164d3710324ba259260e41b6d4818f6142008
OleksandrDreval <180102593+OleksandrDreval@users.noreply.github.com>
2026-02-18 21:39:03 +0200

    feat(modules\security\hardened-services): Add services_systemd-udevd to imports for enhanced security

--- modules/security/hardened-services/default.nix
+++ modules/security/hardened-services/default.nix
@@ -1,17 +1,18 @@
 { config, lib, ... }:
 
+{
   /*
     Hardened services
     Based on: https://github.com/wallago/nix-system-services-hardened
   */
 
-{
   imports = [
     ./accounts-daemon.nix
     ./display-manager.nix
     ./iwd.nix
     ./NetworkManager.nix
     ./NetworkManager-dispatcher.nix
+    ./services_systemd-udevd.nix
     ./sshd.nix
     ./user-session.nix
   ];
