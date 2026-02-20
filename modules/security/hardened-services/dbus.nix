94e800ee3c4da35acc606d52988dc68db088e39d
OleksandrDreval <180102593+OleksandrDreval@users.noreply.github.com>
2026-02-20 23:42:41 +0200

    feat(modules\security\hardened-services\dbus): Add SystemCallErrorNumber to specify EPERM for blocked calls

--- modules/security/hardened-services/dbus.nix
+++ modules/security/hardened-services/dbus.nix
@@ -42,6 +42,7 @@
     # Memory & System Call Filtering
     MemoryDenyWriteExecute = true;       # Prevent W^X memory regions
     SystemCallArchitectures = "native";  # Use only native system calls
+    SystemCallErrorNumber = "EPERM";     # Return EPERM for blocked calls
     SystemCallFilter = [
       "~@obsolete"       # Block deprecated system calls
       "~@resources"      # Block resource limit changes
