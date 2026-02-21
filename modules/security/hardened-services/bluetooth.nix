4219f4d80ebc0dd9b5aeaf89481112244256e7ef
OleksandrDreval <180102593+OleksandrDreval@users.noreply.github.com>
2026-02-21 12:07:22 +0200

    feat(modules\security\hardened-services\bluetooth): Add ProtectHostname setting to prevent hostname changes

--- modules/security/hardened-services/bluetooth.nix
+++ modules/security/hardened-services/bluetooth.nix
@@ -19,6 +19,7 @@
     ProtectKernelTunables = true;  # Make kernel variables (/proc/sys) read-only
     ProtectKernelModules = true;   # Prevent loading/unloading kernel modules
     ProtectKernelLogs = true;      # Prevent reading kernel logs (dmesg)
+    ProtectHostname = true;        # Prevent changing system hostname
     ProtectClock = true;           # Prevent changing system clock
 
     # Memory & System Call Filtering
