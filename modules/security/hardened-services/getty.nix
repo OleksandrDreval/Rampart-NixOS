f3ad15fe92a399eac35338fe9c266b83bef9610f
OleksandrDreval <180102593+OleksandrDreval@users.noreply.github.com>
2026-02-21 12:43:13 +0200

    feat(modules\security\hardened-services\getty): Ensure only native system calls are used for enhanced security

--- modules/security/hardened-services/getty.nix
+++ modules/security/hardened-services/getty.nix
@@ -20,5 +20,6 @@
 
     # Memory & System Call Filtering
     MemoryDenyWriteExecute = true;       # Prevent W^X memory regions
+    SystemCallArchitectures = "native";  # Use only native system calls
   };
 }
