ce7ed4827704577d59bc2fa4f43564fbd123ec57
OleksandrDreval <180102593+OleksandrDreval@users.noreply.github.com>
2026-02-21 13:01:36 +0200

    feat(modules\security\hardened-services\docker): Add real-time scheduling restriction to enhance Docker daemon security

--- modules/security/hardened-services/docker.nix
+++ modules/security/hardened-services/docker.nix
@@ -14,6 +14,8 @@
   systemd.services.docker.serviceConfig = {
     # Privilege & Capability Restrictions
     NoNewPrivileges = true;   # Disallow gaining new privileges
+    RestrictRealtime = true;  # Prevent abuse of real-time scheduling
+    # Block specific capabilities that are highly dangerous or unused by the daemon
     CapabilityBoundingSet = [
       "~CAP_SYS_RAWIO"   # Prevent raw I/O access
       "~CAP_SYS_PTRACE"  # Prevent process tracing
