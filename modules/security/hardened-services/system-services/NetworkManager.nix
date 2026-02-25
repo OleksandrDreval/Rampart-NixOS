2c833788196448f4b0839fc2037c37bff51cbd0f
OleksandrDreval <180102593+OleksandrDreval@users.noreply.github.com>
2026-02-25 20:16:43 +0200

    style(modules\security\hardened-services\system-services\NetworkManager): Correct formatting of RestrictAddressFamilies entries

--- modules/security/hardened-services/system-services/NetworkManager.nix
+++ modules/security/hardened-services/system-services/NetworkManager.nix
@@ -36,11 +36,11 @@
     ProtectProc = "invisible";   # Restrict access to /proc (other processes invisible)
     RestrictNamespaces = true;   # Disable creation of new namespaces
     RestrictAddressFamilies = [
-       "AF_UNIX"     # Local communication
-       "AF_NETLINK"  # Kernel-user communication
-       "AF_INET"     # IPv4
-       "AF_INET6"    # IPv6
-       "AF_PACKET"   # Direct network access (required for NM)
+      "AF_UNIX"     # Local communication
+      "AF_NETLINK"  # Kernel-user communication
+      "AF_INET"     # IPv4
+      "AF_INET6"    # IPv6
+      "AF_PACKET"   # Direct network access (required for NM)
     ];
 
     # Memory & System Call Filtering
