f9c3ff0dd6722cb6914cb07f018abdde844d4030
OleksandrDreval <180102593+OleksandrDreval@users.noreply.github.com>
2026-02-25 18:38:47 +0200

    feat(modules\security\hardened-services\system-services\ModemManager): Add DevicePolicy for enhanced security

--- modules/security/hardened-services/system-services/ModemManager.nix
+++ modules/security/hardened-services/system-services/ModemManager.nix
@@ -81,6 +81,7 @@
     ];
 
     # Other Security Settings
+    DevicePolicy = "auto";    # MM needs access to modem device nodes
     KeyringMode = "private";  # Isolated kernel keyring
     PrivateIPC = true;        # Private IPC namespace
     RemoveIPC = true;         # Clean up IPC objects on service stop
