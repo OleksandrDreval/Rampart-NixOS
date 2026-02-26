b84a74980045f77918116389c67c3196786d5c09
OleksandrDreval <180102593+OleksandrDreval@users.noreply.github.com>
2026-02-26 22:34:36 +0200

    fix(modules\security\hardened-services\system-services\emergency): Move emergency target disabling to the correct position for consistency

--- modules/security/hardened-services/system-services/emergency.nix
+++ modules/security/hardened-services/system-services/emergency.nix
@@ -17,8 +17,8 @@
     an emergency prompt. Recovery will strictly REQUIRE a NixOS Live USB.
   */
 
-  boot.initrd.systemd.targets.emergency.enable = lib.mkForce false;
   boot.initrd.systemd.services.emergency.enable = lib.mkForce false;
+  boot.initrd.systemd.targets.emergency.enable = lib.mkForce false;
 
   systemd.services.emergency.enable = lib.mkForce false;
   systemd.targets.emergency.enable = lib.mkForce false;
